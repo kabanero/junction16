10a6982326d7b1f3b44889d543079cff933ba1cf
Juuso Toikka <jtoikka@gmail.com>
2016-11-26 14:17:13 +0200

    Set enemy, player based on who is host

--- core/src/com/kabanero/junction16/scenes/TestScene.scala
+++ core/src/com/kabanero/junction16/scenes/TestScene.scala
@@ -19,92 +19,100 @@ class TestScene(iAmGood: Boolean) extends Scene {
   val UP = new Vector3(0, 1, 0)
   val RIGHT = new Vector3(-1, 0, 0)
 
+  def ownMovement(delta: Float, node: Node, inputs: AllInputs) {
+    val rotationY = new Quaternion(UP, -inputs.ownInputs.mouseX * CAMERA_SPEED)
+    val rotationX = new Quaternion(RIGHT, -inputs.ownInputs.mouseY * CAMERA_SPEED)
+    node.localRotation.set(rotationY.mul(rotationX))
+
+    val moveDirection = new Vector3(0, 0, 0)
+
+    val forwardMove = node.forward
+    forwardMove.y = 0
+    forwardMove.nor()
+    val rightMove = node.right
+    rightMove.y = 0
+    rightMove.nor()
+    if (inputs.ownInputs.up) {
+      moveDirection.z += 1
+    }
+    if (inputs.ownInputs.down) {
+      moveDirection.z -= 1
+    }
+    if (inputs.ownInputs.left) {
+      moveDirection.x += 1
+    }
+    if (inputs.ownInputs.right) {
+      moveDirection.x -= 1
+    }
+
+    node.localPosition.add(forwardMove.scl(moveDirection.z * delta * PLAYER_SPEED))
+    node.localPosition.add(rightMove.scl(moveDirection.x * delta * PLAYER_SPEED))
+  }
+
+  def otherMovement(delta: Float, node: Node, inputs: AllInputs) {
+    val rotationY = new Quaternion(UP, -inputs.otherInputs.mouseX * CAMERA_SPEED)
+    val rotationX = new Quaternion(RIGHT, -inputs.otherInputs.mouseY * CAMERA_SPEED)
+    node.localRotation.set(rotationY.mul(rotationX))
+
+    val moveDirection = new Vector3(0, 0, 0)
+
+    val forwardMove = node.forward
+    forwardMove.y = 0
+    forwardMove.nor()
+    val rightMove = node.right
+    rightMove.y = 0
+    rightMove.nor()
+    if (inputs.otherInputs.up) {
+      moveDirection.z += 1
+    }
+    if (inputs.otherInputs.down) {
+      moveDirection.z -= 1
+    }
+    if (inputs.otherInputs.left) {
+      moveDirection.x += 1
+    }
+    if (inputs.otherInputs.right) {
+      moveDirection.x -= 1
+    }
+
+    node.localPosition.add(forwardMove.scl(moveDirection.z * delta * PLAYER_SPEED))
+    node.localPosition.add(rightMove.scl(moveDirection.x * delta * PLAYER_SPEED))
+  }
+
   val playerNode = {
 		val node = Node("player")
+    node.localPosition.add(-5.0f, 0, 0)
+
     if (iAmGood) {
-      node.localPosition.add(-5.0f, 0, 0)
+      node.updateMethods += ownMovement
     } else {
-      node.localPosition.add(5.0f, 0, 0)
+      node.updateMethods += otherMovement
+      val modelBuilder = new ModelBuilder()
+      val model = modelBuilder.createBox(
+        1, 1, 1,
+        new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+        Usage.Position | Usage.Normal);
+      val instance = new ModelInstance(model);
+      node.modelInstance = Some(instance);
     }
-
-		node.updateMethods += ((delta: Float, node: Node, inputs: AllInputs) => {
-      val rotationY = new Quaternion(UP, -inputs.ownInputs.mouseX * CAMERA_SPEED)
-      val rotationX = new Quaternion(RIGHT, -inputs.ownInputs.mouseY * CAMERA_SPEED)
-      node.localRotation.set(rotationY.mul(rotationX))
-
-      val moveDirection = new Vector3(0, 0, 0)
-
-      val forwardMove = node.forward
-      forwardMove.y = 0
-      forwardMove.nor()
-      val rightMove = node.right
-      rightMove.y = 0
-      rightMove.nor()
-			if (inputs.ownInputs.up) {
-				moveDirection.z += 1
-			}
-			if (inputs.ownInputs.down) {
-				moveDirection.z -= 1
-			}
-			if (inputs.ownInputs.left) {
-				moveDirection.x += 1
-			}
-			if (inputs.ownInputs.right) {
-				moveDirection.x -= 1
-			}
-
-      node.localPosition.add(forwardMove.scl(moveDirection.z * delta * PLAYER_SPEED))
-      node.localPosition.add(rightMove.scl(moveDirection.x * delta * PLAYER_SPEED))
-		})
-
-		node
-	}
-
+    
+    node
+  }
   val enemyNode = {
     val node = Node("enemy")
+    node.localPosition.add(5.0f, 0, 0)
     if (iAmGood) {
-      node.localPosition.add(5.0f, 0, 0)
+      node.updateMethods += otherMovement
+      val modelBuilder = new ModelBuilder()
+      val model = modelBuilder.createBox(
+        1, 1, 1,
+        new Material(ColorAttribute.createDiffuse(Color.RED)),
+        Usage.Position | Usage.Normal);
+      val instance = new ModelInstance(model);
+      node.modelInstance = Some(instance);
     } else {
-      node.localPosition.add(-5.0f, 0, 0)
+      node.updateMethods += ownMovement
     }
-
-    val modelBuilder = new ModelBuilder()
-    val model = modelBuilder.createBox(
-      1, 1, 1,
-      new Material(ColorAttribute.createDiffuse(Color.RED)),
-      Usage.Position | Usage.Normal);
-    val instance = new ModelInstance(model);
-    node.modelInstance = Some(instance);
-
-    node.updateMethods += ((delta: Float, node: Node, inputs: AllInputs) => {
-      val rotationY = new Quaternion(UP, -inputs.otherInputs.mouseX * CAMERA_SPEED)
-      val rotationX = new Quaternion(RIGHT, -inputs.otherInputs.mouseY * CAMERA_SPEED)
-      node.localRotation.set(rotationY.mul(rotationX))
-
-      val moveDirection = new Vector3(0, 0, 0)
-
-      val forwardMove = node.forward
-      forwardMove.y = 0
-      forwardMove.nor()
-      val rightMove = node.right
-      rightMove.y = 0
-      rightMove.nor()
-			if (inputs.otherInputs.up) {
-				moveDirection.z += 1
-			}
-			if (inputs.otherInputs.down) {
-				moveDirection.z -= 1
-			}
-			if (inputs.otherInputs.left) {
-				moveDirection.x += 1
-			}
-			if (inputs.otherInputs.right) {
-				moveDirection.x -= 1
-			}
-
-      node.localPosition.add(forwardMove.scl(moveDirection.z * delta * PLAYER_SPEED))
-      node.localPosition.add(rightMove.scl(moveDirection.x * delta * PLAYER_SPEED))
-		})
 
 		node
   }
